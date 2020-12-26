75d69d3c2c99916895559f6191c58f4556b95539
Pedro Arthur <pedroarthuralvesifpr1@gmail.com>
2020-12-26 19:38:32 -0300

    Correção da url das imagens do index
    Verificando se os caminhos condizem com a pasta local.

--- Trabalho/js/main.js
+++ Trabalho/js/main.js
@@ -13,7 +13,7 @@ window.onload = function () {
             case 1:
                 document.getElementById('bottom1').innerHTML = 'Hoser'
                 document.getElementById('bottom2').innerHTML = 'Unpleasant person'
-                document.querySelector('.imagem').src = '../img/hoser.jpg'
+                document.querySelector('.imagem').src = '../pages/img/hoser.jpg'
                 audio_ing = new Audio('../sounds/hoser.mp3')
                 document.getElementById('btn').addEventListener('click', function () {
                     audio_ing.play()
@@ -22,7 +22,7 @@ window.onload = function () {
             case 2:
                 document.getElementById('bottom1').innerHTML = 'Loonie'
                 document.getElementById('bottom2').innerHTML = 'Currency, the Canadian dollar.'
-                document.querySelector('.imagem').src = '../img/loonie.jpeg'
+                document.querySelector('.imagem').src = '../pages/img/loonie.jpeg'
                 audio_ing = new Audio('../sounds/loonie.mp3')
                 document.getElementById('btn').addEventListener('click', function () {
                     audio_ing.play()
@@ -31,7 +31,7 @@ window.onload = function () {
             case 3:
                 document.getElementById('bottom1').innerHTML = 'Kerfuffle'
                 document.getElementById('bottom2').innerHTML = 'Slang to say that a situation is too crazy or confusing.'
-                document.querySelector('.imagem').src = '../img/Kerfuffle.jpeg'
+                document.querySelector('.imagem').src = '../pages/img/Kerfuffle.jpeg'
                 audio_ing = new Audio('../sounds/kerfuffle.mp3')
                 document.getElementById('btn').addEventListener('click', function () {
                     audio_ing.play()
@@ -40,7 +40,7 @@ window.onload = function () {
             case 4:
                 document.getElementById('bottom1').innerHTML = 'Mawga'
                 document.getElementById('bottom2').innerHTML = 'When a person is not feeling very well'
-                document.querySelector('.imagem').src = '../img/Mawga.jpeg'
+                document.querySelector('.imagem').src = '../pages/img/Mawga.jpeg'
                 audio_ing = new Audio('../sounds/mawga.mp3')
                 document.getElementById('btn').addEventListener('click', function () {
                     audio_ing.play()
@@ -49,7 +49,7 @@ window.onload = function () {
             case 5:
                 document.getElementById('bottom1').innerHTML = 'Owly'
                 document.getElementById('bottom2').innerHTML = 'When a person is in a bad mood'
-                document.querySelector('.imagem').src = '../img/Owly.jpg'
+                document.querySelector('.imagem').src = '../pages/img/Owly.jpg'
                 audio_ing = new Audio('../sounds/owly.mp3')
                 document.getElementById('btn').addEventListener('click', function () {
                     audio_ing.play()
@@ -58,7 +58,7 @@ window.onload = function () {
             case 6:
                 document.getElementById('bottom1').innerHTML = 'Runners'
                 document.getElementById('bottom2').innerHTML = 'Specific tennis for running and gym.'
-                document.querySelector('.imagem').src = '../img/Runners.jpeg'
+                document.querySelector('.imagem').src = '../pages/img/Runners.jpeg'
                 audio_ing = new Audio('../sounds/runners.mp3')
                 document.getElementById('btn').addEventListener('click', function () {
                     audio_ing.play()
@@ -67,7 +67,7 @@ window.onload = function () {
             case 7:
                 document.getElementById('bottom1').innerHTML = 'Take Off'
                 document.getElementById('bottom2').innerHTML = 'Used to talk that will leave some place suddenly.'
-                document.querySelector('.imagem').src = '../img/TakeOff.jpeg'
+                document.querySelector('.imagem').src = '../pages/img/TakeOff.jpeg'
                 audio_ing = new Audio('../sounds/taken.mp3')
                 document.getElementById('btn').addEventListener('click', function () {
                     audio_ing.play()
@@ -76,7 +76,7 @@ window.onload = function () {
             case 8:
                 document.getElementById('bottom1').innerHTML = 'Weather In'
                 document.getElementById('bottom2').innerHTML = 'Expression used to talk about bad weather.'
-                document.querySelector('.imagem').src = '../img/Weatherin.jpeg'
+                document.querySelector('.imagem').src = '../pages/img/Weatherin.jpeg'
                 audio_ing = new Audio('../sounds/weather.mp3')
                 document.getElementById('btn').addEventListener('click', function () {
                     audio_ing.play()
@@ -85,7 +85,7 @@ window.onload = function () {
             case 9:
                 document.getElementById('bottom1').innerHTML = 'Washroom'
                 document.getElementById('bottom2').innerHTML = 'Bathroom in Canadian.'
-                document.querySelector('.imagem').src = '../img/Washroom.jpg'
+                document.querySelector('.imagem').src = '../pages/img/Washroom.jpg'
                 audio_ing = new Audio('../sounds/washroom.mp3')
                 document.getElementById('btn').addEventListener('click', function () {
                     audio_ing.play()
@@ -94,7 +94,7 @@ window.onload = function () {
             case 10:
                 document.getElementById('bottom1').innerHTML = 'Toque'
                 document.getElementById('bottom2').innerHTML = 'Type of cap made with very popular knitting'
-                document.querySelector('.imagem').src = '../img/toque.jpeg'
+                document.querySelector('.imagem').src = '../pages/img/toque.jpeg'
                 audio_ing = new Audio('../sounds/toque.mp3')
                 document.getElementById('btn').addEventListener('click', function () {
                     audio_ing.play()
@@ -103,7 +103,7 @@ window.onload = function () {
             case 11:
                 document.getElementById('bottom1').innerHTML = 'Pop'
                 document.getElementById('bottom2').innerHTML = 'Canadian English version for soda.'
-                document.querySelector('.imagem').src = '../img/pop.jpg'
+                document.querySelector('.imagem').src = '../pages/img/pop.jpg'
                 audio_ing = new Audio('../sounds/pop.mp3')
                 document.getElementById('btn').addEventListener('click', function () {
                     audio_ing.play()
@@ -113,7 +113,7 @@ window.onload = function () {
             case 12:
                 document.getElementById('bottom1').innerHTML = 'Double-double'
                 document.getElementById('bottom2').innerHTML = 'Coffee made with two cubes of sugar and two layers of cream.'
-                document.querySelector('.imagem').src = '../img/double.jpg'
+                document.querySelector('.imagem').src = '../pages/img/double.jpg'
                 audio_ing = new Audio('../sounds/double-double.mp3')
                 document.getElementById('btn').addEventListener('click', function () {
                     audio_ing.play()
@@ -122,7 +122,7 @@ window.onload = function () {
             case 13:
                 document.getElementById('bottom1').innerHTML = 'Poutine'
                 document.getElementById('bottom2').innerHTML = 'They are fries with cheese cubes and spice sauce'
-                document.querySelector('.imagem').src = '../img/poutine.jpg'
+                document.querySelector('.imagem').src = '../pages/img/poutine.jpg'
                 audio_ing = new Audio('../sounds/poutine.mp3')
                 document.getElementById('btn').addEventListener('click', function () {
                     audio_ing.play()
